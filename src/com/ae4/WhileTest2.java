c2e0ba9a682c8cb0b146c13d90aa9bf329d619db
JeonYijin <juan6016@naver.com>
2021-07-15 10:26:32 +0900

    Update WhileTest2.java

--- src/com/ae4/WhileTest2.java
+++ src/com/ae4/WhileTest2.java
@@ -26,12 +26,16 @@ public class WhileTest2 {
 				ppw = sc.nextInt();
 				if(iid==id && ppw ==pw) {
 					System.out.println("로그인 성공");
-					flag = !flag;
+					//flag = !flag;
+					break;
+				}else {
+					System.out.println("로그인 실패");
 				}
 				
 			}else {
 				flag = !flag;
 			}
+			System.out.println(flag);
 			
 			//로그인 성공, 로그인 안됐거나 로그인이 성공했으면 게임을 시작
 			//최초레벨 1, 만렙 15 모든 몬스터의 경험치는 동일
@@ -43,52 +47,46 @@ public class WhileTest2 {
 			//레벨 3-> 4 : 9마리 4->5 12 5-> 6 15마리 14 ->15 : 42
 			//15되면 종료 최종 레벨 출력과 골드 출력 후 종료
 			
-			int level = 1;
-			int gold = 0;
-			int monster = 0;
-			// 레벨업 시 지급되는 골드
-			int add = 1000;
-			
-			if(iid==id && ppw==pw) {
-				for(level = 1; level<15; level++) {
-					for(int i =0; i< level*3; i++) {
-						
-						monster ++;
-						System.out.println(monster+"마리를 잡았습니다.");
-					} 
-						//monster = 0; 여기 있었는데 왜 초기화는 맨 마지막에??
-					
-					if(monster%3 ==0) {
-						//level을 업그레이드 한 상태로 출력하기 위해++
-						level++;
-						System.out.println(level+"레벨 업");
-					
-						if(level%5 ==0) {
-							gold = gold + add;
-							System.out.println(add +"골드를 지급합니다.");
-							add = add+1000;
-						}
-						monster = 0;
-						//다시 for문으로 돌아갔을 때 수가 달라지지 않게하기 위해
-						level = level-1;
-						
-					}
-				}	
-				System.out.println("최종레벨: " + level+ " 골드: " + gold);
-				System.out.println("게임을 종료합니다.");
-				break;
-
-			//해결하고 싶은 점 1. add라는 변수 없이 gold만을 가지고 증가시키기
-			//2. monster의 초기화, level을 찍을 때의 문제점 +1을 안할 수 있는 방법
-			//3. 불필요한 level의 증감
-			
-		}
-		
-		
-		
 		
 		System.out.println("WhileTest2 finish");
 	}
 
+		int level = 1;
+		int gold = 0;
+		int monster = 0;
+		// 레벨업 시 지급되는 골드
+		int add = 1000;
+		//코드가 끝날때 꼭 삭제 - 로그인 판단
+		flag = false;
+		if(!flag) {
+			System.out.println("로그인 성공 게임 시작");
+			for(level = 1; level<15; level++) {
+				for(monster = 0 ; monster< level*3; monster++){
+					System.out.println(monster+1+"마리를 잡았습니다.");
+				}
+				System.out.println("레벨 업! 현재 레벨:"+(level+1));
+				
+				////골드 지급
+					level++;
+				if(level%5 ==0) {
+					gold = gold + add;
+					System.out.println(add +"골드를 지급합니다.");
+					add = add+1000;
+				} level = level-1;
+				
+				
+			}
+			System.out.println("최종레벨: " + level+ " 골드: " + gold);
+			System.out.println("게임을 종료합니다.");
+
+	}else {
+		System.out.println("로그인 실패로 종료되었습니다.");
+	}
+	
+	
+	
+		
+		
+		
 	}
 }
