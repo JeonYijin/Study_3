628d374c4611c266a6d20b6086e1f1a02873ac2a
JeonYijin <juan6016@naver.com>
2021-07-16 17:29:44 +0900

    Update Array_8.java

--- src/com/ae5/Array_8.java
+++ src/com/ae5/Array_8.java
@@ -23,6 +23,7 @@ public class Array_8 {
 			System.out.println("번호선택 >>");
 			//입력한 번호 num
 			int num = sc.nextInt();
+			boolean check = true;
 			//로그인을 시도한다면
 			if(num==1) {
 				//아이디와 비밀번호 입력받기
@@ -30,15 +31,20 @@ public class Array_8 {
 				int id = sc.nextInt();
 				System.out.println("비밀번호를 입력");
 				int pw = sc.nextInt();
+				
 				//입력한 id와 pw 일치하는지 확인
 				for(int i=0; i<ids.length; i++) {
 					if(id == ids[i] && pw==pws[i]) {
 						System.out.println("로그인 성공");
 						break;
 					}else {
-						continue;
+						check = !check;
 					}
 				}
+				if(check==false) {
+					System.out.println("로그인실패");
+				}
+				
 			}else if(num ==2) {
 				//새로운 id를 담을 배열 만들기
 				int [] nids = new int[ids.length+1];
@@ -46,17 +52,23 @@ public class Array_8 {
 				System.out.println("아이디를 설정하세요>>");
 				int nid = sc.nextInt();
 				//id를 새로 만들어서 원래 배열에 담기
-				for(int i=0; i<ids.length; i++) {
-					if(nid != ids[i]) {
-						nids[i] = ids[i];
-						
-					}else {
-						System.out.println("중복된 아이디입니다. 다시 입력하세요.");
-						nid = sc.nextInt();
+				for(int i =0; i<ids.length; i++) {
+					if(nid == ids[i]) {
+						System.out.println("중복된 아이디입니다.");
+						check = false;
 					}
-				} 	nids[ids.length] = nid;
-					ids = nids;
-				
+				}	//아이디가 중복일때 회원가입 실패하고 처음으로 돌아가기
+				if(check==false) {
+					System.out.println("회원가입실패");
+					continue;
+				}else {//중복아니면 배열에 담기
+					for(int i=0; i<ids.length; i++) {
+						nids[i] = ids[i];
+					}
+						nids[ids.length] = nid;
+						ids = nids;
+				}
+					
 				//새로운 pw를 담을 배열 만들기
 				int[] npws = new int[pws.length+1];
 				
@@ -67,11 +79,7 @@ public class Array_8 {
 						npws[i] = pws[i];
 				} 	npws[pws.length] = npw;
 					pws = npws;
-//					for(int i =0; i<ids.length; i++) {
-//						System.out.print(ids[i] +" ");
-//						System.out.print(pws[i]+ " ");
-//					}
-//				
+
 			}else {
 				System.out.println("종료합니다.");
 				flag = false;
